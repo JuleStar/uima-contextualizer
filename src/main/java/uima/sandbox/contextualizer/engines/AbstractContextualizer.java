a86879441487adeb0219a42e936f04d6d388532f
Damien Cram <damien.cram@univ-nantes.fr>
2016-01-27 12:05:50 +0100

    Removed uima-profiler dependency

--- src/main/java/uima/sandbox/contextualizer/engines/AbstractContextualizer.java
+++ src/main/java/uima/sandbox/contextualizer/engines/AbstractContextualizer.java
@@ -2,27 +2,23 @@ package uima.sandbox.contextualizer.engines;
 
 import java.util.List;
 
-import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
-import org.apache.uima.resource.ResourceInitializationException;
 
 import uima.sandbox.contextualizer.models.Context;
-import fr.univnantes.lina.UIMAProfiler;
 
 
 public abstract class AbstractContextualizer extends JCasAnnotator_ImplBase {
-
+	
 	public static final String SCOPE = "Scope";
 	@ConfigurationParameter(name = SCOPE, mandatory = true)
 	private int scope;
 	
 	@Override
 	public void process(JCas cas) throws AnalysisEngineProcessException {
-		UIMAProfiler.getProfiler("AnalysisEngine").start(this, "process");
 		try {
 			Context context = new Context(cas,this.scope);
 			List<List<Annotation>> lists = context.make();
@@ -39,7 +35,6 @@ public abstract class AbstractContextualizer extends JCasAnnotator_ImplBase {
 		} catch (Exception e) {
 			throw new AnalysisEngineProcessException(e);
 		}
-		UIMAProfiler.getProfiler("AnalysisEngine").stop(this, "process");
 	}
 
 	protected abstract void annotate(JCas cas,Annotation annotation, Annotation[] annotations); 
